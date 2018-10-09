8096ea8cfd437e16a0d2df6a70fab48000acc5bd
sestalin <crjleonardo5@gmail.com>
2018-10-09 16:02:09 -0400

    Counting remaining days

--- Tiempo y Espacio/TimeAndSpace.cs
+++ Tiempo y Espacio/TimeAndSpace.cs
@@ -9,15 +9,19 @@ namespace Tiempo_y_Espacio
 {
     class TimeAndSpace
     {
-
+        int DaysOfYear = 365;
         int[] monthDays = new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 
         public void Manager()
         {
             int day;
-            int month;
             int year;
+            int month;
+            int dayOfYear;
+            string nextDay;
+            int remainingDays;
+
             string date = "";
             Match validation;
             Regex regex = new Regex(@"^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$");
@@ -29,23 +33,34 @@ namespace Tiempo_y_Espacio
             {
                 if (validation.Success)
                 {
-                    day = int.Parse(date.Substring(0, 2));
-                    month = int.Parse(date.Substring(3, 2));
-                    year = int.Parse(date.Substring(6, 4));
+                    day = int.Parse(date.Substring(0,2));
+                    month = int.Parse(date.Substring(3,2));
+                    year = int.Parse(date.Substring(6,4));
 
 
                     if (IsLeapYear(year))
                     {
                         monthDays[1] = 29;
-                        Console.WriteLine("Año Bisiesto");
+                        DaysOfYear = 366;
+                        Console.WriteLine("\nAño Bisiesto");
                     }
                         
                     else
-                        Console.WriteLine("Año No Bisiesto");
-                    
-                    Console.WriteLine("Día del año: " + GetDayOfYear(day, month, year).ToString());
-                    Console.WriteLine("Días restantes del año: " + RemainingYearDays(day, month, year).ToString());
-                    Console.WriteLine("Día siguiente: " + GetNextDay(day, month, year).ToString());
+                        Console.WriteLine("\nAño No Bisiesto");
+
+
+                    nextDay = GetNextDay(day, month, year);
+                    dayOfYear = GetDayOfYear(day, month, year);
+                    remainingDays = RemainingYearDays(day, month, year);
+
+                    Console.WriteLine("\nDía del año: " + dayOfYear.ToString());
+                    Console.WriteLine("\nDía siguiente: " + nextDay);
+                    Console.WriteLine("\nTotal días restantes del año: " + remainingDays.ToString());
+
+                    Console.WriteLine("\nDías restantes del año: \n");
+
+                    for(int i = dayOfYear+1; i<= DaysOfYear; i++)
+                        Console.Write(i+" ");
 
 
                     Console.ReadKey();
@@ -54,17 +69,18 @@ namespace Tiempo_y_Espacio
             }
             catch (Exception e)
             {
-                Console.WriteLine("Exception: " + e);
+                Console.WriteLine("Utilice el formato DD/MM/YYYY. ejemplo: 01/02/1995 \n");
             }
             finally
             {
+                DaysOfYear = 365;
                 monthDays[1] = 28;
                 Manager();
             }
 
         }
 
-        public bool IsLeapYear(int year)
+        bool IsLeapYear(int year)
         {
 
             if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
@@ -74,7 +90,7 @@ namespace Tiempo_y_Espacio
 
         }
 
-        public int GetDayOfYear(int day, int month, int year)
+        int GetDayOfYear(int day, int month, int year)
         {
             int count = 0;
             int dayOfyear = 0;
@@ -88,7 +104,7 @@ namespace Tiempo_y_Espacio
             return dayOfyear;
         }
 
-        public string GetNextDay(int day, int month, int year)
+        string GetNextDay(int day, int month, int year)
         {
             if (day<monthDays[month-1])
             {
@@ -114,7 +130,7 @@ namespace Tiempo_y_Espacio
             return day+"/"+month+"/"+year;
         }
         
-        public int RemainingYearDays(int day, int month, int year)
+        int RemainingYearDays(int day, int month, int year)
         {
             return GetDayOfYear(31, 12, year) - GetDayOfYear(day, month, year);
         }
